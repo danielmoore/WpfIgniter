890a4c1e10031c01afb50a93b1dca40d7089ae49
danielmoore <daniel@northhorizon.net>
2011-02-20 13:07:48 -0500

    Initial checkin

---
+++ InpcTemplate.Tests/BindableBaseTest.cs
@@ -0,0 +1,103 @@
+﻿using InpcTemplate;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace InpcTemplate.Tests
+{
+
+
+	/// <summary>
+	///This is a test class for BindableBaseTest and is intended
+	///to contain all BindableBaseTest Unit Tests
+	///</summary>
+	[TestClass()]
+	public class BindableBaseTest
+	{
+		private TestBindableType _sut;
+
+		#region Additional test attributes
+		// 
+		//You can use the following additional attributes as you write your tests:
+		//
+		//Use ClassInitialize to run code before running the first test in the class
+		[ClassInitialize()]
+		public static void MyClassInitialize(TestContext testContext)
+		{
+		}
+		//
+		//Use ClassCleanup to run code after all tests in a class have run
+		//[ClassCleanup()]
+		//public static void MyClassCleanup()
+		//{
+		//}
+		//
+		//Use TestInitialize to run code before running each test
+		//[TestInitialize()]
+		//public void MyTestInitialize()
+		//{
+		//}
+		//
+		//Use TestCleanup to run code after each test has run
+		//[TestCleanup()]
+		//public void MyTestCleanup()
+		//{
+		//}
+		//
+		#endregion
+
+
+		internal virtual BindableBase_Accessor CreateBindableBase_Accessor()
+		{
+			// TODO: Instantiate an appropriate concrete class.
+			BindableBase_Accessor target = null;
+			return target;
+		}
+
+		/// <summary>
+		///A test for OnPropertyChanging
+		///</summary>
+		[TestMethod()]
+		[DeploymentItem("InpcTemplate.dll")]
+		public void OnPropertyChangingTest()
+		{
+			PrivateObject param0 = null; // TODO: Initialize to an appropriate value
+			BindableBase_Accessor target = new BindableBase_Accessor(param0); // TODO: Initialize to an appropriate value
+			string propertyName = string.Empty; // TODO: Initialize to an appropriate value
+			target.OnPropertyChanging(propertyName);
+			Assert.Inconclusive("A method that does not return a value cannot be verified.");
+		}
+
+		/// <summary>
+		///A test for OnPropertyChanged
+		///</summary>
+		[TestMethod()]
+		[DeploymentItem("InpcTemplate.dll")]
+		public void OnPropertyChangedTest()
+		{
+			PrivateObject param0 = null; // TODO: Initialize to an appropriate value
+			BindableBase_Accessor target = new BindableBase_Accessor(param0); // TODO: Initialize to an appropriate value
+			string propertyName = string.Empty; // TODO: Initialize to an appropriate value
+			target.OnPropertyChanged(propertyName);
+			Assert.Inconclusive("A method that does not return a value cannot be verified.");
+		}
+
+		/// <summary>
+		///A test for VerifyCallerIsProperty
+		///</summary>
+		[TestMethod()]
+		[DeploymentItem("InpcTemplate.dll")]
+		public void VerifyCallerIsPropertyTest()
+		{
+			PrivateObject param0 = null; // TODO: Initialize to an appropriate value
+			BindableBase_Accessor target = new BindableBase_Accessor(param0); // TODO: Initialize to an appropriate value
+			string propertyName = string.Empty; // TODO: Initialize to an appropriate value
+			target.VerifyCallerIsProperty(propertyName);
+			Assert.Inconclusive("A method that does not return a value cannot be verified.");
+		}
+
+		private class TestBindableType : BindableBase
+		{
+
+		}
+	}
+}
---
+++ InpcTemplate/BindableBase.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Diagnostics;
+
+namespace InpcTemplate
+{
+	public abstract class BindableBase : INotifyPropertyChanged, INotifyPropertyChanging
+	{
+		public event PropertyChangedEventHandler PropertyChanged = delegate { };
+		public event PropertyChangingEventHandler PropertyChanging = delegate { };
+
+		protected void SetProperty<T>(ref T backingStore, T value, string propertyName, Action onChanged = null, Action<T> onChanging = null)
+		{
+			VerifyCallerIsProperty(propertyName);
+
+			if (EqualityComparer<T>.Default.Equals(backingStore, value)) return;
+
+			if (onChanging != null) onChanging(value);
+
+			OnPropertyChanging(propertyName);
+
+			backingStore = value;
+
+			if (onChanged != null) onChanged();
+
+			OnPropertyChanged(propertyName);
+		}
+
+		[Conditional("DEBUG")]
+		private void VerifyCallerIsProperty(string propertyName)
+		{
+			var stackTrace = new StackTrace();
+			var frame = stackTrace.GetFrames()[2];
+			var caller = frame.GetMethod();
+
+			if (!caller.Name.Equals(propertyName, StringComparison.InvariantCulture))
+				throw new InvalidOperationException(string.Format("Called SetProperty for {0} from {1}", propertyName, caller.Name));
+		}
+
+		protected virtual void OnPropertyChanged(string propertyName)
+		{
+			PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+		protected virtual void OnPropertyChanging(string propertyName)
+		{
+			PropertyChanging(this, new PropertyChangingEventArgs(propertyName));
+		}
+	}
+}
