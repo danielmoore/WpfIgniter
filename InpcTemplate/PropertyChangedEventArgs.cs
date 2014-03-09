ea04486564a2b7995a8402f94e148cb4ced2d0eb
Daniel Moore <polaris@northhorizon.net>
2014-03-09 16:19:54 -0400

    adding XML docs for PropertyChangedEventArgs<T> and privatizing the properties

--- InpcTemplate/PropertyChangedEventArgs.cs
+++ InpcTemplate/PropertyChangedEventArgs.cs
@@ -2,8 +2,18 @@
 
 namespace NorthHorizon.Samples.InpcTemplate
 {
+    /// <summary>
+    /// Provides typed value change information for the <see cref="INotifyPropertyChanged.PropertyChanged"/> event.
+    /// </summary>
+    /// <typeparam name="T">The type of the property that changed.</typeparam>
     public class PropertyChangedEventArgs<T> : PropertyChangedEventArgs
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PropertyChangedEventArgs{T}"/> class.
+        /// </summary>
+        /// <param name="propertyName">Name of the property.</param>
+        /// <param name="oldValue">The old value of the property.</param>
+        /// <param name="newValue">The new value of the property.</param>
         public PropertyChangedEventArgs(string propertyName, T oldValue, T newValue)
             : base(propertyName)
         {
@@ -11,7 +21,14 @@ namespace NorthHorizon.Samples.InpcTemplate
             NewValue = newValue;
         }
 
-        public T OldValue { get; set; }
-        public T NewValue { get; set; }
+        /// <summary>
+        /// Gets the old value of the property.
+        /// </summary>
+        public T OldValue { get; private set; }
+
+        /// <summary>
+        /// Gets the new value of the property.
+        /// </summary>
+        public T NewValue { get; private set; }
     }
 }
