7f00a83882f50f6b42c69236ff864b0b7444afa4
Daniel Moore <polaris@northhorizon.net>
2014-09-13 22:02:41 +0100

    Changed WeakEvent to keep a reference of the subscribing delegate bound to the subscribing target. Fixes #7

---
+++ Igniter.Tests/Core/WeakEventTest.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Text;
+using Igniter.Core;
+using NUnit.Framework;
+
+namespace Igniter.Tests.Core
+{
+    [TestFixture]
+    public class WeakEventTest
+    {
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CannotAddNonDelegate()
+        {
+            var ev = new WeakEvent<string>();
+
+            ev.Add("foo");
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CannotAddClosure()
+        {
+            var ev = new WeakEvent<Action>();
+
+            var builder = new StringBuilder();
+            ev.Add(() => builder.Append("foobar"));
+        }
+
+        [Test]
+        public void CanAddStaticLambda()
+        {
+            var ev = new WeakEvent<Action>();
+
+            ev.Add(() => TestStaticMethod());
+        }
+
+        [Test]
+        public void CanAddThisClosure()
+        {
+            var ev = new WeakEvent<Action>();
+
+            ev.Add(() => TestInstanceMethod());
+        }
+
+        [Test]
+        public void SubscribersCanBeCollected()
+        {
+            var ev = new WeakEvent<Action>();
+
+            var sub = new Subscriber();
+            var subRef = new WeakReference(sub);
+
+            ev.Add(sub.Call);
+
+            sub = null;
+
+            GC.Collect();
+
+            var callCount = 0;
+            ev.Invoke(h => { 
+                callCount++;
+                h();
+            });
+
+            callCount.ShouldEqual(0);
+            subRef.Target.Should(Be.Null);
+        }
+
+        [Test]
+        public void SubscribersAreCalled()
+        {
+            var ev = new WeakEvent<Action>();
+
+            var sub = new Subscriber();
+
+            ev.Add(sub.Call);
+
+            GC.Collect();
+
+            var callCount = 0;
+            ev.Invoke(h =>
+            {
+                callCount++;
+                h();
+            });
+
+            callCount.ShouldEqual(1);
+            sub.CallCount.ShouldEqual(1);
+        }
+
+        [Test]
+        public void SubscribersCanBeRemoved()
+        {
+            var ev = new WeakEvent<Action>();
+
+            var sub = new Subscriber();
+
+            ev.Add(sub.Call);
+
+            ev.Remove(sub.Call);
+
+            GC.Collect();
+
+            var callCount = 0;
+            ev.Invoke(h =>
+            {
+                callCount++;
+                h();
+            });
+
+            callCount.ShouldEqual(0);
+            sub.CallCount.ShouldEqual(0);
+        }
+        
+        private class Subscriber
+        {
+            public int CallCount { get; set; }
+
+            public void Call()
+            {
+                CallCount++;
+            }
+        }
+
+        private static int TestInstanceMethod()
+        {
+            return -1;
+        }
+
+        public static int TestStaticMethod()
+        {
+            return -1; 
+        }
+    }
+}
--- Igniter/Core/WeakEvent.cs
+++ Igniter/Core/WeakEvent.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Runtime.CompilerServices;
 
 namespace Igniter.Core
 {
@@ -10,7 +11,36 @@ namespace Igniter.Core
     /// <typeparam name="TDelegate">The type of the delegate.</typeparam>
     public sealed class WeakEvent<TDelegate> where TDelegate : class
     {
-        private readonly List<WeakReference> _delegates = new List<WeakReference>();
+        private readonly ConditionalWeakTable<object, List<TDelegate>> _delegates = new ConditionalWeakTable<object, List<TDelegate>>();
+        private readonly List<WeakReference> _invocationLists = new List<WeakReference>();
+        private readonly List<TDelegate> _staticInvocationList = new List<TDelegate>();
+
+        private List<TDelegate> GetInvocationList(object target)
+        {
+            if (target == null) return _staticInvocationList;
+
+            List<TDelegate> invocationList;
+            if (!_delegates.TryGetValue(target, out invocationList))
+            {
+                _delegates.Add(target, invocationList = new List<TDelegate>(5));
+                _invocationLists.Add(new WeakReference(invocationList));
+            }
+
+            return invocationList;
+        }
+
+        private static object GetTarget(TDelegate handler)
+        {
+            var @delegate = handler as Delegate;
+
+            if (@delegate == null)
+                throw new ArgumentException("Handler is not a delegate", "handler");
+
+            if (@delegate.Target != null && Attribute.IsDefined(@delegate.Method.DeclaringType, typeof(CompilerGeneratedAttribute)))
+                throw new ArgumentException("Handler is compiler generated and cannot be added to a weak event", "handler");
+
+            return @delegate.Target;
+        }
 
         /// <summary>
         /// Adds the specified event handler to the invocation list.
@@ -21,8 +51,9 @@ namespace Igniter.Core
         {
             if (handler == null) throw new ArgumentNullException("handler");
 
-            lock (_delegates)
-                _delegates.Add(new WeakReference(handler));
+            var target = GetTarget(handler);
+
+            GetInvocationList(target).Add(handler);
         }
 
         /// <summary>
@@ -34,18 +65,9 @@ namespace Igniter.Core
         {
             if (handler == null) throw new ArgumentNullException("handler");
 
-            lock (_delegates)
-                for (int i = _delegates.Count - 1; i >= 0; i--)
-                {
-                    var current = _delegates[i].Target;
-                    if (current == null)
-                        _delegates.RemoveAt(i);
-                    else if (current.Equals(handler))
-                    {
-                        _delegates.RemoveAt(i);
-                        break;
-                    }
-                }
+            var target = GetTarget(handler);
+
+            GetInvocationList(target).Remove(handler);
         }
 
         /// <summary>
@@ -57,15 +79,30 @@ namespace Igniter.Core
         {
             if (invoker == null) throw new ArgumentNullException("invoker");
 
-            lock (_delegates)
-                for (int i = _delegates.Count - 1; i >= 0; i--)
+            var removed = 0;
+
+            var count = _invocationLists.Count;
+            for (int i = 0; i < count; i++)
+            {
+                var invocationList = (List<TDelegate>)_invocationLists[i].Target;
+
+                if (invocationList == null)
+                    removed++;
+                else
                 {
-                    var current = _delegates[i].Target;
-                    if (current == null)
-                        _delegates.RemoveAt(i);
-                    else
-                        invoker((TDelegate)current);
+                    for (var j = 0; j < invocationList.Count; j++)
+                        invoker(invocationList[j]);
+
+                    _invocationLists[i - removed] = _invocationLists[i];
                 }
+            }
+
+            if (removed > 0)
+                _invocationLists.RemoveRange(count - removed, removed);
+            
+            int staticCount = _staticInvocationList.Count;
+            for (int i = 0; i < staticCount; i++)
+                invoker(_staticInvocationList[i]);
         }
     }
 }
