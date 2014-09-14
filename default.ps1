20311b07ffa39b3271fd27a23da8b742a4ce8281
Daniel Moore <polaris@northhorizon.net>
2014-09-14 16:51:12 +0100

    updating release script to commit version entry

--- default.ps1
+++ default.ps1
@@ -27,6 +27,8 @@ task Update-Version {
 task Set-Tag {
 	$version = Get-Version
 
+	Exec { git commit allow-empty -m "v$version" }
+	Exec { git push }
 	Exec { git tag v/$version }
 	Exec { git push origin tag v/$version }
 }
