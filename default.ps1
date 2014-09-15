55984ef07eac180ce5bddfc3bc5cede42b988bb1
Daniel Moore <polaris@northhorizon.net>
2014-09-15 03:28:45 +0100

    Adding release task to build script

--- default.ps1
+++ default.ps1
@@ -14,34 +14,78 @@ task Clean {
     Run-MsBuild 'Clean'
 }
 
-task Update-Version {
+task Update-CompiledVersion {
     Get-Version 
 
-    Write-Host "Setting version to '$($Script:version)'"
+    Write-Host "Setting compiled version to '$($Script:version)'"
 
     Expand-Template $asmInfoTemplatePath {
         $version = $Script:version
+		
+		$dotNetVersion = ($version -split '-')[0]
     }
+}
+
+task Update-PackageVersion {
+    Write-Host "Setting packaged version to '$($Script:version)'"
+
+	[xml]$nuspec = Get-Content $nuspecPath
+
+	$nuspec.package.metadata.version = "$Script:version"
+
+	$writeSettings = new-Object -TypeName 'Xml.XmlWriterSettings'
+
+	$writeSettings.Indent = $true
+	$writeSettings.IndentChars = "  "
+
+	Exec { 
+		try { 
+			$writer = [Xml.XmlWriter]::Create($nuspecPath, $writeSettings)
+			$nuspec.Save($writer) 
+		} finally {
+			if ($writer -ne $null) {
+				$writer.Dispose();
+			}
+		}
+	}
+}
+
+task Read-Version {
+	while ($version -eq $null) {
+		$currentVersion = Get-Version 
+
+		$version = Read-Host "New Version (Currently $currentVersion)"
+	}
+
+	$Script:version = $version
+}
+
+task Set-Version -depends Read-Version, Update-CompiledVersion, Update-PackageVersion {
+	$version = Get-Version
+	
+	Exec { git commit -am "Updating version to $version" }
 }
 
 task Set-Tag {
 	$version = Get-Version
 
-	Exec { git tag v/$version }
-	Exec { git push origin tag v/$version }
+	Exec { git tag v/$version --force }
+	Exec { git push origin tag v/$version --force }
 }
 
 task Compile -depends Clean {
     Run-MsBuild 'Build'
 }
 
-task Package -depends Clean, Update-Version, Compile {
+task Package -depends Clean, Update-CompiledVersion, Compile {
     Exec { nuget pack $nuspecPath -OutputDirectory $outDir }
 }
 
 task Publish -depends Package, Set-Tag {
     Exec { nuget push (Join-Path $outDir '*.nupkg') -NonInteractive }
 }
+
+task Release -depends Publish, Set-Version
 
 task default -depends Compile
 
