e35437bfea73526093330b130fa18d2cb1207c2b
Daniel Moore <polaris@northhorizon.net>
2014-09-24 02:37:58 +0100

    Added ItemPositionBehavior to library

---
+++ Igniter.Tests.Live/ItemPositionTestWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Igniter.Tests.Live
+{
+    /// <summary>
+    /// Interaction logic for ItemPositionTestWindow.xaml
+    /// </summary>
+    [LiveTest("ItemPositionBehavior")]
+    public partial class ItemPositionTestWindow : Window
+    {
+        public ItemPositionTestWindow()
+        {
+            DataContext = new ItemPositionViewModel();
+
+            InitializeComponent();
+        }
+    }
+}
