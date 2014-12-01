8f5e83d458ea494b03101f2a4d061955dd2d9230
Daniel Moore <polaris@northhorizon.net>
2014-12-01 07:45:25 -0500

    Updating version to 2.3.0

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,12 +1,11 @@
 ﻿using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Windows.Markup;
 
 [assembly: AssemblyProduct("Igniter")]
 [assembly: AssemblyConfiguration("Release")]
 [assembly: AssemblyCompany("North Horizon")]
 [assembly: AssemblyCopyright("Copyright © Daniel Moore 2014")]
-[assembly: AssemblyCulture("")] // main (not satellite) assembly, no culture. 
+[assembly: AssemblyCulture("")] // main (not satellite) assembly, no culture.
 
-[assembly: AssemblyVersion("2.2.1")]
+[assembly: AssemblyVersion("2.3.0")]
+[assembly: AssemblyInformationalVersion("2.3.0")]
 
